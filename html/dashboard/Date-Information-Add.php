ef7498d992ee6c5b9618cdda5e0074b9b5948ec3
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-06-14 15:00:55 +0700

    update

--- html/dashboard/Date-Information-Add.php
+++ html/dashboard/Date-Information-Add.php
@@ -179,7 +179,6 @@
                                     <h1>Hello Devs!</h1>
                                     <p>We are on a mission to help developers like you build successful projects for FREE.</p>
                                 </div>
-
                             </div>
                         </div>
                     </div>
@@ -194,167 +193,60 @@
                 </div>
             </div> <!-- Nav Header Component End -->
         </div>
+        <!--Nav End-->
         <div class="conatiner-fluid content-inner mt-n5 py-0">
             <div>
                 <div class="row">
-                    <div class="col-xl-3 col-lg-4">
+                    <div class="col-sm-12 col-lg-12">
                         <div class="card">
                             <div class="card-header d-flex justify-content-between">
                                 <div class="header-title">
-                                    <h4 class="card-title">University Information</h4>
+                                    <h4 class="card-title">New University</h4>
                                 </div>
-
                             </div>
-                            <?php
-                            if (isset($_GET['university_id'])) {
-                                require_once 'connect.php';
-                                $stmt = $conn->prepare("SELECT * FROM university WHERE university_id=?");
-                                $stmt->execute([$_GET['university_id']]);
-                                $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                                $university = $row['university'];
-                            }
-                            ?>
                             <div class="card-body">
-                                <form>
-                                    <div class="form-group">
-                                        <label class="form-label" for="University">University</label>
-                                        <input type="text" class="form-control" value="<?= $row['university']; ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label" for="Department">Department</label>
-                                        <input type="text" class="form-control" value="<?= $row['department']; ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label" for="Country">Country</label>
-                                        <input type="text" class="form-control" value="<?= $row['country']; ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="form-label" for="QS Ranking">QS Ranking</label>
-                                        <input type="text" class="form-control" value="<?= $row['ranking']; ?>">
-                                    </div>
-                                    <div class="form-group mb-0">
-                                        <label class="form-label" for="QS Ranking by Subject">QS Ranking by Subject</label>
-                                        <input type="text" class="form-control" value="<?= $row['qs_suject']; ?>"">
-                                    </div>
-                                    <div class=" form-group mb-0">
-                                        <label class="form-label" for="Specialization">Specialization</label>
-                                        <input type="text" class="form-control" value="<?= $row['spec']; ?>">
-                                    </div>
-                                    <div class="form-group mb-0">
-                                        <label class="form-label" for="Comments">Comments</label>
-                                        <input type="text" class="form-control" value="<?= $row['comments_u']; ?>">
-                                    </div>
-                                    <div class="form-group mb-0">
-                                        <label class="form-label" for="MOU/MOA">MOU/MOA</label>
-                                        <input type="text" class="form-control" value="<?= $row['mou']; ?>">
-                                    </div>
-                                    <div class="form-group mb-0">
-                                        <label class="form-label" for="Active">Active</label>
-                                        <input type="text" class="form-control" value="<?= $row['expired']; ?> / <?= $row['signed']; ?>">
-                                    </div>
+                                <form id="form-wizard1" class="mt-3 text-center">
+                                    <fieldset>
+                                        <div class="form-card text-start">
+                                            <div class="row">
+                                                <input type="text" name="university_id" value="<?= $row['university_id']; ?>" hidden>
+                                                <input type="text" name="university_name" value="<?= $row['university']; ?>" hidden>
+                                                <input type="text" name="department" value="<?= $row['department']; ?>" hidden>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="form-label">Start *</label>
+                                                        <input type="date" name="date_s" required class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="form-label">End *</label>
+                                                        <input type="date" name="date_s" required class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="form-label">Activity types *</label>
+                                                        <input type="text" name="activity" required class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="form-label">Name Surname *</label>
+                                                        <input type="text" name="name" required class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label class="form-label">Details *</label>
+                                                        <textarea class="form-control" name="details" style="height: 150px"></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button type="button" class="btn btn-primary  action-button float-end">Next</button>
+                                    </fieldset>
                                 </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-9 col-lg-8">
-                        <div class="card">
-                            <div class="card-header d-flex justify-content-between">
-                                <div class="header-title">
-                                    <h4 class="card-title">Information</h4>
-                                </div>
-                                <div>
-                                    <a href="Date-Information-Add.php" class="btn btn-link btn-soft-light">
-                                        Add Information
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <div class="new-user-info">
-                                    <table id="datatable" class="table table-striped" data-toggle="data-table">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Detail</th>
-                                                <th>Start</th>
-                                                <th>End</th>
-                                                <th>Activity types</th>
-                                                <th>Agreement Details</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            require_once 'connect.php';
-                                            $stmt = $conn->prepare("SELECT * FROM dateinter WHERE university_id=:university_id");
-                                            $stmt->bindParam(':university_id', $row['university_id'], PDO::PARAM_STR);
-                                            $stmt->execute();
-                                            $result = $stmt->fetchAll();
-                                            $countrow = 1;
-                                            foreach ($result as $t1) {
-                                            ?>
-                                                <tr>
-                                                    <td> <?= $countrow ?></td>
-                                                    <td>
-                                                        <div class="flex align-items-center list-user-action">
-                                                            <a class="btn btn-sm btn-icon btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="View" href="Date-Information-View.php?id=<?= $t1['id']; ?>">
-                                                                <span class="btn-inner">
-                                                                    <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                        <path d="M15.7161 16.2234H8.49609" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                        <path d="M15.7161 12.0369H8.49609" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                        <path d="M11.2521 7.86011H8.49707" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M15.909 2.74976C15.909 2.74976 8.23198 2.75376 8.21998 2.75376C5.45998 2.77076 3.75098 4.58676 3.75098 7.35676V16.5528C3.75098 19.3368 5.47298 21.1598 8.25698 21.1598C8.25698 21.1598 15.933 21.1568 15.946 21.1568C18.706 21.1398 20.416 19.3228 20.416 16.5528V7.35676C20.416 4.57276 18.693 2.74976 15.909 2.74976Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                    </svg>
-                                                                </span>
-                                                            </a>
-                                                            <a class="btn btn-sm btn-icon btn-warning" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit" href="Date-Information-Edit.php?id=<?= $t1['id']; ?>">
-                                                                <span class="btn-inner">
-                                                                    <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                        <path d="M11.4925 2.78906H7.75349C4.67849 2.78906 2.75049 4.96606 2.75049 8.04806V16.3621C2.75049 19.4441 4.66949 21.6211 7.75349 21.6211H16.5775C19.6625 21.6211 21.5815 19.4441 21.5815 16.3621V12.3341" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8.82812 10.921L16.3011 3.44799C17.2321 2.51799 18.7411 2.51799 19.6721 3.44799L20.8891 4.66499C21.8201 5.59599 21.8201 7.10599 20.8891 8.03599L13.3801 15.545C12.9731 15.952 12.4211 16.181 11.8451 16.181H8.09912L8.19312 12.401C8.20712 11.845 8.43412 11.315 8.82812 10.921Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                        <path d="M15.1655 4.60254L19.7315 9.16854" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                    </svg>
-                                                                </span>
-                                                            </a>
-                                                            <a class="btn btn-sm btn-icon btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" href="Date-University-Add.php?university_id=<?= $t1['university_id']; ?>">
-                                                                <span class="btn-inner">
-                                                                    <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
-                                                                        <path d="M19.3248 9.46826C19.3248 9.46826 18.7818 16.2033 18.4668 19.0403C18.3168 20.3953 17.4798 21.1893 16.1088 21.2143C13.4998 21.2613 10.8878 21.2643 8.27979 21.2093C6.96079 21.1823 6.13779 20.3783 5.99079 19.0473C5.67379 16.1853 5.13379 9.46826 5.13379 9.46826" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                        <path d="M20.708 6.23975H3.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                        <path d="M17.4406 6.23973C16.6556 6.23973 15.9796 5.68473 15.8256 4.91573L15.5826 3.69973C15.4326 3.13873 14.9246 2.75073 14.3456 2.75073H10.1126C9.53358 2.75073 9.02558 3.13873 8.87558 3.69973L8.63258 4.91573C8.47858 5.68473 7.80258 6.23973 7.01758 6.23973" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                    </svg>
-                                                                </span>
-                                                            </a>
-                                                        </div>
-                                                    </td>
-                                                    <?php
-                                                    setlocale(LC_TIME, 'en_US'); // Set the locale to English (United States)
-                                                    $date_s_formatted = strftime('%d %b %Y', strtotime($t1['date_s']));
-                                                    ?>
-                                                    <td><?php echo $date_s_formatted; ?></td>
-                                                    <?php
-                                                    setlocale(LC_TIME, 'en_US'); // Set the locale to English (United States)
-                                                    $date_e_formatted = strftime('%d %b %Y', strtotime($t1['date_e']));
-                                                    ?>
-                                                    <td><?php echo $date_e_formatted; ?></td>
-                                                    <td><?= $t1['activity']; ?></td>
-                                                    <td><?= nl2br($t1['details']); ?></td>
-                                                </tr>
-                                            <?php
-                                                $countrow++;
-                                            }
-                                            ?>
-                                        </tbody>
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Detail</th>
-                                                <th>Start</th>
-                                                <th>End</th>
-                                                <th>Activity types</th>
-                                                <th>Agreement Details</th>
-                                            </tr>
-                                        </thead>
-                                    </table>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -389,6 +281,7 @@
         </svg>
     </a>
     <!-- Wrapper End-->
+
     <!-- offcanvas start -->
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" data-bs-scroll="true" data-bs-backdrop="true" aria-labelledby="offcanvasExampleLabel">
         <div class="offcanvas-header">

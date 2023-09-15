403534d406059ccf1a7a4b159d8ad82b60f4f72d
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-09-15 14:47:01 +0700

    updata

--- Date-University.php
+++ Date-University.php
@@ -210,15 +210,35 @@
                                                                 </svg>
                                                             </span>
                                                         </a>
-                                                        <a class="btn btn-sm btn-icon btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" href="Del-University.php?university_id=<?= $t1['university_id']; ?>">
-                                                            <span class="btn-inner">
+                                                        <a class="btn btn-sm btn-icon btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" href="javascript:void(0);" onclick="confirmDelete('<?= $t1['university_id']; ?>')"><span class="btn-inner">
                                                                 <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
                                                                     <path d="M19.3248 9.46826C19.3248 9.46826 18.7818 16.2033 18.4668 19.0403C18.3168 20.3953 17.4798 21.1893 16.1088 21.2143C13.4998 21.2613 10.8878 21.2643 8.27979 21.2093C6.96079 21.1823 6.13779 20.3783 5.99079 19.0473C5.67379 16.1853 5.13379 9.46826 5.13379 9.46826" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                                                                     <path d="M20.708 6.23975H3.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                                                                     <path d="M17.4406 6.23973C16.6556 6.23973 15.9796 5.68473 15.8256 4.91573L15.5826 3.69973C15.4326 3.13873 14.9246 2.75073 14.3456 2.75073H10.1126C9.53358 2.75073 9.02558 3.13873 8.87558 3.69973L8.63258 4.91573C8.47858 5.68473 7.80258 6.23973 7.01758 6.23973" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                                                                 </svg>
-                                                            </span>
-                                                        </a>
+                                                            </span></a>
+                                                        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+                                                        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+                                                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+                                                        <script>
+                                                            function confirmDelete(university_id) {
+                                                                swal({
+                                                                        title: "Are you sure?",
+                                                                        text: "Do you want to delete it? Once deleted, it cannot be recovered.",
+                                                                        type: "warning",
+                                                                        showCancelButton: true,
+                                                                        confirmButtonColor: "#DD6B55",
+                                                                        confirmButtonText: "Yes, cancel it!",
+                                                                        cancelButtonText: "No",
+                                                                        closeOnConfirm: false
+                                                                    },
+                                                                    function(isConfirm) {
+                                                                        if (isConfirm) {
+                                                                            window.location = "Del-University.php?university_id=" + university_id;
+                                                                        }
+                                                                    });
+                                                            }
+                                                        </script>
                                                     </div>
                                                 </td>
                                             </tr>
